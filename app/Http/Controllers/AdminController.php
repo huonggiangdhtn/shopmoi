fdb8b79a2873032ee65b3bf13c8d109f51f49fbc
Huongson06 <hson17135@gmail.com>
2024-11-08 08:17:07 +0700

    #8 - Upload_dev_main3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,15 +19,35 @@ class AdminController extends Controller
         //
        
         $func = "admin_view";
-        if(!$this->check_function($func))
-        {
-            return redirect()->route('home');
-        }
-        $data['breadcrumb'] = '
-        <li class="breadcrumb-item"><a href="#">/</a></li>
-        <li class="breadcrumb-item active" aria-current="page"> Bảng điều khiển</li>';
-       $data['active_menu']='dashboard';
-        return view ('backend.index',   $data);
+          if(!$this->check_function($func))
+          {
+              return redirect()->route('home');
+          }
+          $data['breadcrumb'] = '
+          <li class="breadcrumb-item"><a href="#">/</a></li>
+          <li class="breadcrumb-item active" aria-current="page"> Bảng điều khiển</li>';
+          $data['active_menu']="dashboard";
+          $month = date('m');
+          $year = date('Y');
+          $day = date('d');
+          $lastmonth = $month - 1;
+          $lastyear = $year;
+          if($lastmonth <= 0)
+          {
+                    $lastmonth = 12;
+                    $lastyear = $year - 1;
+          }
+         
+          $sql1 = "select count(id) as tong from blogs where status = 'active'  ";
+          $data['sobai'] = \DB::select($sql1)[0]->tong;
+          $sql2 = "select count(id) as tong from orders where status = 'active'  ";
+          $data['sodon'] = \DB::select($sql2)[0]->tong;
+          $sql3 = "select sum(final_amount) as tong from orders where status = 'active'  ";
+          $data['tongdon'] = \DB::select($sql3)[0]->tong;
+
+          $data['hotproducts']=\DB::select('select * from products order by hit desc limit 10');
+          $data['logs'] = \App\Models\Log::orderBy('id','desc')->limit(20)->get();
+          return view ('backend.index',   $data);
    
         // echo 'i am admin';
     }
